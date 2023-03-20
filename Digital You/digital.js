602551be06eb450d93a61763a8ff8495e818f108
CarlosMonroy182 <124704879+CarlosMonroy182@users.noreply.github.com>
2023-03-19 18:17:38 -0600

    Add files via upload

---
+++ Digital You/digital.js
@@ -0,0 +1,158 @@
+$(document).ready(function () {
+    
+
+
+$(".btn1").click(function () { 
+    $(".process2").css({
+        "background-image": "url(./assets/tarjetas.jpg)"
+    });
+    
+});
+
+$(".btn2").click(function () { 
+    $(".process2").css({
+        "background-image": "url(./assets/tarjetas2.jpg)"
+    });
+    
+});
+
+$(".btn3").click(function () { 
+    $(".process2").css({
+        "background-image": "url(./assets/tarjetas3.jpg)"
+    });
+    
+});
+
+$(".btn4").click(function () { 
+    $(".process2").css({
+        "background-image": "url(./assets/tarjetas4.jpg)"
+    });
+    
+});
+
+$(".btn1").click(function () { 
+    $(".txt1").css({
+        "color" : "#fff"
+    });
+});
+
+$(".btn1").mouseleave(function () { 
+    $(".txt1").css({
+        "color":"#7A7A7A"
+    });
+});
+
+$(".btn2").click(function () { 
+    $(".txt2").css({
+        "color" : "#fff"
+    });
+});
+
+$(".btn2").mouseleave(function () { 
+    $(".txt2").css({
+        "color":"#7A7A7A"
+    });
+});
+
+$(".btn3").click(function () { 
+    $(".txt3").css({
+        "color" : "#fff"
+    });
+});
+
+$(".btn3").mouseleave(function () { 
+    $(".txt3").css({
+        "color":"#7A7A7A"
+    });
+});
+
+
+$(".btn4").click(function () { 
+    $(".txt4").css({
+        "color" : "#fff"
+    });
+});
+
+$(".btn4").mouseleave(function () { 
+    $(".txt4").css({
+        "color":"#7A7A7A"
+    });
+});
+
+
+$(".icono-menu").click(function () { 
+    $(".navbar-res").css({
+        "right":"0",
+        "transition": "0.5s",
+    });
+    
+});
+
+$(".close").click(function () { 
+    $(".navbar-res").css({
+        "right":"-100vw",
+        "transition": "0.5s", 
+    });
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".navbar").click(function () { 
+//     $(".navbar").css({
+//         "left" : "0%",
+//         "transition" : "0.8s",
+//     });
+
+    
+// });
+
+// $(".navbar").mouseleave(function () { 
+//     $(".navbar").css({
+//         "left" : "25%",
+//         "transition" : "0.8s",
+//     });
+
+// });
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
